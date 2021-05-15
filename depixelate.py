0e7b9b512a85b21f98a0d03cb943d8bb33073099
Abraham Calvillo <31970808+Abruuham@users.noreply.github.com>
2021-05-14 23:10:48 -0700

    Initial Commit

--- depixelate.py
+++ depixelate.py
@@ -7,7 +7,6 @@ def pixelate_images(self):
     original_path = Path("original_images")
     pixelated_path = Path("pixelated_images")
 
-    # pixelated_path.mkdir(exist_ok=True)
 
     for png_file in glob.glob('**/*.png'):
         full_file_path = str(png_file)
